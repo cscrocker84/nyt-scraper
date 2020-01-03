62f7128e85ff087b25689d6c32271b72bfc5431f
Charlie Crocker <cscrocker84@gmail.com>
2020-01-03 11:03:41 -0600

    Finished the schemas, exporting them with index.js

---
+++ models/Note.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// reference to schema constructor
+const Schema = mongoose.Schema;
+
+// creates new NoteSchema
+const NoteSchema = new Schema({
+    body: String
+});
+
+// creates model from ArticleSchema
+const Note = mongoose.model("Note", NoteSchema)
+
+// export Article to app.js
+module.exports = Note;
